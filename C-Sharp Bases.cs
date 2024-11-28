fe3ba6edf13614a5a23caff0f2bcd689085f3113
VRS-Z <omendes222@gmail.com>
2024-11-28 09:30:21 -0300

    add exercise of Fahrenheit for Celsius

--- C-Sharp Bases.cs
+++ C-Sharp Bases.cs
@@ -17,3 +17,10 @@ Console.WriteLine("Second decrement: " + value);
 
 value--;
 Console.WriteLine("Third decrement: " + value);
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
---
+++ Challenger_Calculate_Celsius_in_Fahrenheit.cs
@@ -0,0 +1,3 @@
+int fahrenheit = 94;
+int celsius = (fahrenheit - 32) * 5/9;
+Console.WriteLine($"The temperature is {celsius} Celsius.");
